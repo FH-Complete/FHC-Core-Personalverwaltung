c4ed3f0f97769a1e1665272232d043d0d588ff0a
Werner Masik <werner@gefi.at>
2022-07-12 17:58:26 +0200

    fixed sql

--- sql/1/001_hr_tbl_sachaufwand.sql
+++ sql/1/001_hr_tbl_sachaufwand.sql
@@ -1,4 +1,4 @@
-CREATE TABLE hr.tbl_sachaufwand (
+CREATE TABLE IF NOT EXISTS hr.tbl_sachaufwand (
 	sachaufwand_id bigint NOT NULL,
 	mitarbeiter_uid character varying(32),
 	sachaufwandtyp_kurzbz character varying(32),
@@ -22,15 +22,21 @@ CREATE SEQUENCE IF NOT EXISTS hr.tbl_sachaufwand_sachaufwand_id_seq
 
 GRANT SELECT, UPDATE ON hr.tbl_sachaufwand_sachaufwand_id_seq TO vilesci;
 
-ALTER TABLE hr.tbl_sachaufwand ALTER COLUMN sachaufwand_id SET DEFAULT nextval('hr.tbl_sachaufwand_sachaufwand_id_seq');
+DO $$
+BEGIN
+	ALTER TABLE hr.tbl_sachaufwand ALTER COLUMN sachaufwand_id SET DEFAULT nextval('hr.tbl_sachaufwand_sachaufwand_id_seq');
+ 	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
 
+DO $$
+BEGIN
+	ALTER TABLE hr.tbl_sachaufwand ADD CONSTRAINT tbl_mitarbeiter_fk FOREIGN KEY (mitarbeiter_uid)
+	REFERENCES public.tbl_mitarbeiter (mitarbeiter_uid) MATCH FULL
+	ON DELETE SET NULL ON UPDATE CASCADE;
+ 	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
 
-
-ALTER TABLE hr.tbl_sachaufwand ADD CONSTRAINT tbl_mitarbeiter_fk FOREIGN KEY (mitarbeiter_uid)
-REFERENCES public.tbl_mitarbeiter (mitarbeiter_uid) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
-
-CREATE TABLE hr.tbl_sachaufwandtyp (
+CREATE TABLE IF NOT EXISTS hr.tbl_sachaufwandtyp (
 	sachaufwandtyp_kurzbz character varying(32) NOT NULL,
 	bezeichnung character varying(256),
 	sort smallint,
@@ -38,14 +44,39 @@ CREATE TABLE hr.tbl_sachaufwandtyp (
 	CONSTRAINT tbl_sachaufwandtyp_pk PRIMARY KEY (sachaufwandtyp_kurzbz)
 );
 
-ALTER TABLE hr.tbl_sachaufwand ADD CONSTRAINT tbl_sachaufwandtyp_fk FOREIGN KEY (sachaufwandtyp_kurzbz)
-REFERENCES hr.tbl_sachaufwandtyp (sachaufwandtyp_kurzbz) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
+DO $$
+BEGIN
+	ALTER TABLE hr.tbl_sachaufwand ADD CONSTRAINT tbl_sachaufwandtyp_fk FOREIGN KEY (sachaufwandtyp_kurzbz)
+	REFERENCES hr.tbl_sachaufwandtyp (sachaufwandtyp_kurzbz) MATCH FULL
+	ON DELETE SET NULL ON UPDATE CASCADE;
+    EXCEPTION WHEN OTHERS THEN NULL;
+END $$;	
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hr.tbl_sachaufwand TO vilesci;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hr.tbl_sachaufwandtyp TO vilesci;
 
-INSERT INTO hr.tbl_sachaufwandtyp (sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) VALUES('Jobticket','Jobticket',1,true);
-INSERT INTO hr.tbl_sachaufwandtyp (sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) VALUES('Klimaticket','Klimaticket',2,true);
-INSERT INTO hr.tbl_sachaufwandtyp (sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) VALUES('Pendlerpauschale','Pendlerpauschale',3,true);
+INSERT INTO hr.tbl_sachaufwandtyp 
+	(sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) 
+SELECT 'Jobticket','Jobticket',1,true
+WHERE
+   NOT EXISTS (
+       SELECT sachaufwandtyp_kurzbz FROM hr.tbl_sachaufwandtyp WHERE sachaufwandtyp_kurzbz = 'Jobticket'
+   );
+
+INSERT INTO hr.tbl_sachaufwandtyp 
+	(sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) 
+SELECT 'Klimaticket','Klimaticket',2,true
+WHERE
+   NOT EXISTS (
+       SELECT sachaufwandtyp_kurzbz FROM hr.tbl_sachaufwandtyp WHERE sachaufwandtyp_kurzbz = 'Klimaticket'
+   );
+
+
+INSERT INTO hr.tbl_sachaufwandtyp 
+	(sachaufwandtyp_kurzbz, bezeichnung, sort, aktiv) 
+SELECT 'Pendlerpauschale','Pendlerpauschale',3,true
+WHERE
+   NOT EXISTS (
+       SELECT sachaufwandtyp_kurzbz FROM hr.tbl_sachaufwandtyp WHERE sachaufwandtyp_kurzbz = 'Pendlerpauschale'
+   );
