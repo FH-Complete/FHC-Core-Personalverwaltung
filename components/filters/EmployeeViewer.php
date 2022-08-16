a19b2aad0566b96e449df936358ee438d015dc0b
Harald Bamberger <harald.bamberger@technikum-wien.at>
2022-08-16 14:47:01 +0200

    add Kostenstelle

--- components/filters/EmployeeViewer.php
+++ components/filters/EmployeeViewer.php
@@ -21,6 +21,7 @@
 				ma.ort_kurzbz as "Raum",
 				ma.geschlecht as "Geschlecht",				
 				ma.telefonklappe as "DW",
+				kst.bezeichnung as "StdKst", 
 				oe.oe_kurzbz as "OeKurzbz",
 				oe.oe_parent_kurzbz as "OeParent",
 				oe.bezeichnung as "OeBezeichnung",
@@ -30,8 +31,11 @@
 				SELECT bf.uid,oe_kurzbz,oe_parent_kurzbz,public.tbl_organisationseinheit.bezeichnung,organisationseinheittyp_kurzbz  
 				FROM public.tbl_benutzerfunktion bf JOIN public.tbl_organisationseinheit using(oe_kurzbz)
 				WHERE funktion_kurzbz=\'oezuordnung\' and datum_von<=now() AND (datum_bis is null OR datum_bis>=now())) oe USING(uid)
+			   LEFT JOIN (
+				SELECT bf.uid,oe_kurzbz,public.tbl_organisationseinheit.bezeichnung
+				FROM public.tbl_benutzerfunktion bf JOIN public.tbl_organisationseinheit using(oe_kurzbz)
+				WHERE funktion_kurzbz=\'kstzuordnung\' and datum_von<=now() AND (datum_bis is null OR datum_bis>=now())) kst USING(uid)
 		 ORDER BY ma.nachname, ma.vorname
 		',
 		'requiredPermissions' => 'basis/mitarbeiter'
 	);
-
