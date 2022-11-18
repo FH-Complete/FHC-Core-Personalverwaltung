a52aaf24fa9b56e0866ad2089553dd301d76afcf
Werner Masik <werner@gefi.at>
2022-11-18 11:01:30 +0100

    added m to n relation for valorisierung

--- sql/1/007_hr_tbl_valorisierung.sql
+++ sql/1/007_hr_tbl_valorisierung.sql
@@ -1,17 +1,23 @@
+
 CREATE TABLE IF NOT EXISTS hr.tbl_valorisierung (
-    valorisierung_id integer NOT NULL,
-    jahr date,
+    valorisierung_id integer NOT NULL,    
+    datum date NOT NULL,
     prozent numeric(8,4),
     betrag numeric(8,2),
-    dienstverhaeltnis_id integer,
-    CONSTRAINT "betrag or prozent" CHECK (((betrag IS NULL) OR (prozent IS NULL)))
+    insertamum timestamp,
+	insertvon character varying(32),
+	updateamum timestamp,
+	updatevon character varying(32),
+    CONSTRAINT "betrag or prozent" CHECK (((betrag IS NULL) OR (prozent IS NULL))),
+    CONSTRAINT valorisierung_id_pk PRIMARY KEY (valorisierung_id)
 );
 
 CREATE SEQUENCE IF NOT EXISTS hr.tbl_valorisierung_valorisierung_id_seq
     INCREMENT BY 1
     NO MAXVALUE
     NO MINVALUE
-    CACHE 1;
+    CACHE 1
+    OWNED BY hr.tbl_valorisierung.valorisierung_id;
 
 GRANT SELECT, UPDATE ON hr.tbl_valorisierung_valorisierung_id_seq TO vilesci;
 
@@ -23,11 +29,53 @@ END $$;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hr.tbl_valorisierung TO vilesci;
 
+--
+
+CREATE TABLE IF NOT EXISTS hr.tbl_valorisierung_dienstverhaeltnis (
+    valorisierung_dienstverhaeltnis_id integer NOT NULL,
+    valorisierung_id integer NOT NULL, 
+    dienstverhaeltnis_id integer NOT NULL,
+    angewendet boolean default false,
+    insertamum timestamp,
+	insertvon character varying(32),
+	updateamum timestamp,
+	updatevon character varying(32),
+    CONSTRAINT valorisierung_dienstverhaeltnis_id_pk PRIMARY KEY (valorisierung_dienstverhaeltnis_id)
+);    
+
+CREATE SEQUENCE IF NOT EXISTS hr.tbl_valorisierung_dv_valorisierung_dv_id_seq
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1
+    OWNED BY hr.tbl_valorisierung_dienstverhaeltnis.valorisierung_dienstverhaeltnis_id;
+
+GRANT SELECT, UPDATE ON hr.tbl_valorisierung_dv_valorisierung_dv_id_seq TO vilesci;
 
 DO $$
 BEGIN
-	ALTER TABLE hr.tbl_valorisierung ADD CONSTRAINT tbl_dienstverhaeltnis_fk FOREIGN KEY (dienstverhaeltnis_id)
-	REFERENCES hr.tbl_dienstverhaeltnis (dienstverhaeltnis_id) MATCH FULL
-	ON DELETE SET NULL ON UPDATE CASCADE;
+	ALTER TABLE hr.tbl_valorisierung_dienstverhaeltnis ALTER COLUMN valorisierung_dienstverhaeltnis_id SET DEFAULT nextval('hr.tbl_valorisierung_dv_valorisierung_dv_id_seq');
  	EXCEPTION WHEN OTHERS THEN NULL;
 END $$;
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hr.tbl_valorisierung_dienstverhaeltnis TO vilesci;
+
+
+DO $$
+BEGIN
+	ALTER TABLE hr.tbl_valorisierung_dienstverhaeltnis ADD CONSTRAINT tbl_dienstverhaeltnis_fk FOREIGN KEY (dienstverhaeltnis_id)
+	REFERENCES hr.tbl_dienstverhaeltnis (dienstverhaeltnis_id) MATCH FULL
+	ON DELETE CASCADE ON UPDATE CASCADE;
+ 	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
+
+DO $$
+BEGIN
+	ALTER TABLE hr.tbl_valorisierung_dienstverhaeltnis ADD CONSTRAINT tbl_valorisierung_fk FOREIGN KEY (valorisierung_id)
+	REFERENCES hr.tbl_valorisierung (valorisierung_id) MATCH FULL
+	ON DELETE CASCADE ON UPDATE CASCADE;
+ 	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
+
+
+
