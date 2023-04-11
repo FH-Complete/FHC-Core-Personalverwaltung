a3869aa462fb3f9c1aff192c74f6b70f03d596e4
Werner Masik <werner@gefi.at>
2023-04-11 12:42:16 +0200

    fixed date validation

--- libraries/gui/util.php
+++ libraries/gui/util.php
@@ -1,9 +1,9 @@
 <?php
 
+
 function string2Date($datestring)
 {
-    //$date = DateTimeImmutable::createFromFormat('j.m.Y', $datestring);
-    //if ($date === false) return null;
-    //return $date->format('Y-m-d');
-    return $datestring;
+    $date = DateTimeImmutable::createFromFormat('Y-m-d', $datestring);
+    if ($date === false) return null;
+    return $date->format('Y-m-d');
 }
