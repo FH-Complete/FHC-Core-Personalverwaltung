00ce832af8437ff4e3a196abaec1919b6bf727a0
Cris <hainberg@technikum-wien.at>
2023-11-23 11:24:34 +0100

    Removed shadow from menu bar

--- views/reports/home.php
+++ views/reports/home.php
@@ -31,7 +31,7 @@
 
 <div id="wrapper">
 
-  <header class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <header class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0 border-bottom">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="<?php echo APP_ROOT ?>">FHComplete [PV21]</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
