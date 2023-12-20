130c16e7806d02217dd3e27f768024c4c2af29ac
Harald Bamberger <harald.bamberger@technikum-wien.at>
2023-12-20 15:41:21 +0100

    use APP_ROOT instead of BASE_LOCATION

--- controllers/apis/TabsConfig.php
+++ controllers/apis/TabsConfig.php
@@ -29,15 +29,15 @@ class TabsConfig extends Auth_Controller
 		$result = array(
 			'test1' => array(
 				'title' => 'Test1',
-				'component' => BASE_LOCATION . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
 			),
 			'test2' => array(
 				'title' => 'Test2',
-				'component' => BASE_LOCATION . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
 			),
 			'test3' => array(
 				'title' => 'Test3',
-				'component' => BASE_LOCATION . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
 			)
 		);
 
