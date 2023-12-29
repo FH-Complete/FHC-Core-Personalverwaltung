7b79e43a43b8b921c4153f82e9d515a45affdeb4
Werner Masik <werner@gefi.at>
2023-12-29 16:28:49 +0100

    make employee base data  components work with Tabs.js

--- controllers/apis/TabsConfig.php
+++ controllers/apis/TabsConfig.php
@@ -19,6 +19,15 @@ class TabsConfig extends Auth_Controller
 				'Stammdaten' => self::DEFAULT_PERMISSION
 			)
 		);
+
+		// Loads phrases system
+		$this->loadPhrases(
+			array(
+				'global',
+				'ui',
+				'person'
+			)
+		);
     }
 
     function index()
@@ -26,18 +35,42 @@ class TabsConfig extends Auth_Controller
 
     function Stammdaten()
     {
+			
 		$result = array(
-			'test1' => array(
-				'title' => 'Test1',
-				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+			'base' => array(
+				'title' => $this->p->t('global', 'stammdaten'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/BaseData.js',
+				'config' => null
 			),
-			'test2' => array(
-				'title' => 'Test2',
-				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+			'employee' => array(
+				'title' => $this->p->t('person', 'mitarbeiterdaten'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/EmployeeData.js',
+				'config' => null
 			),
-			'test3' => array(
-				'title' => 'Test3',
-				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/organisation/chartdemo.js'
+			'contact' => array(
+				'title' => $this->p->t('person', 'kontaktdaten'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/contact/ContactData.js',
+				'config' => null
+			),
+			'bank' => array(
+				'title' => $this->p->t('person', 'bankdaten'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/BankData.js',
+				'config' => null
+			),
+			'materialexpenses' => array(
+				'title' => $this->p->t('person', 'sachaufwand'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/MaterialExpensesData.js',
+				'config' => null
+			),
+			'hourlyrate' => array(
+				'title' => $this->p->t('person', 'stundensaetze'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/HourlyRateData.js',
+				'config' => null
+			),
+			'jobfunction' => array(
+				'title' => $this->p->t('person', 'funktionen'),
+				'component' => APP_ROOT . 'public/extensions/FHC-Core-Personalverwaltung/js/components/employee/JobFunction.js',
+				'config' => null
 			)
 		);
 
--- public/js/components/employee/EmployeePerson.js
+++ public/js/components/employee/EmployeePerson.js
@@ -16,8 +16,8 @@ export const EmployeePerson = {
 		ContactData,
 		MaterialExpensesData,
 		HourlyRateData: HourlyRateData,
-                JobFunction,
-                FhcTabs
+        JobFunction,
+        FhcTabs,
 	},
     props: {
         // personid: { type: Number, default: 0 }
@@ -31,6 +31,8 @@ export const EmployeePerson = {
         const { t } = usePhrasen();
         const items = ["base", "employee", "contact", "bank", "material", "hourly", "funktion"];
         const activeItem = Vue.ref("base");
+        const theModel = Vue.reactive({personID: null, personUID: null,editmode: true});
+       //const theModel = Vue.ref('masik');
        // const { personID } = Vue.toRefs(props);
 
         const isActive = (menuItem) => {
@@ -45,140 +47,44 @@ export const EmployeePerson = {
         const updateHeaderHandler = () => {
             emit('updateHeader')
         }
-
+/*
         Vue.watch(
 			() => route.params,
 			params => {
 				currentPersonID.value = parseInt(params.id);
                 currentPersonUID.value = params.uid;
+                theModel.personID = currentPersonID.value;
+                theModel.personUID = currentPersonUID.value;
 			}
-		)
+		) */
 
         currentPersonID.value = parseInt(route.params.id);
         currentPersonUID.value = route.params.uid;
+        theModel.personID = parseInt(route.params.id);
+        theModel.personUID = route.params.uid;
 /*
         Vue.onMounted(() => {
             currentPersonID.value = parseInt(route.params.id);
             currentPersonUID.value = route.params.uid;
         })
 */
-        return { items, isActive, setActive, currentPersonID, currentPersonUID, t, updateHeaderHandler }
+        return { items, isActive, setActive, currentPersonID, currentPersonUID, t, updateHeaderHandler,theModel }
 
     },
     template: `
     <div class="d-flex justify-content-between align-items-center ms-sm-auto col-lg-12 p-md-2">
       <div class="container-fluid">
     
-        <fhc-tabs config-url="/extensions/FHC-Core-Personalverwaltung/apis/TabsConfig/Stammdaten" style="flex: 1 1 0%; height: 0%"></fhc-tabs>
-    
-        <div class="row pt-md-2">
-            
-            <nav class="nav flex-column col-md-2 subsubnav">
-                <a
-                    class="nav-link"
-                    :class="{ active: isActive(items[0]) }"
-                    @click.prevent="setActive(items[0])"
-                    href="#base"
-                >{{ t('global', 'stammdaten') }}</a
-                >
-                <a
-                    class="nav-link"
-                    :class="{ active: isActive(items[1]) }"
-                    @click.prevent="setActive(items[1])"
-                    href="#employee"
-                    >{{ t('person', 'mitarbeiterdaten') }}</a
-                >
-                <a
-                    class="nav-link"
-                    :class="{ active: isActive(items[2]) }"
-                    @click.prevent="setActive(items[2])"
-                    href="#contact"
-                    >{{ t('person', 'kontaktdaten') }}</a
-                >
-                <a
-                    class="nav-link"
-                    :class="{ active: isActive(items[3]) }"
-                    @click.prevent="setActive(items[3])"
-                    href="#bank"
-                    >{{ t('person', 'bankdaten') }}</a
-                >
-                <a
-                    class="nav-link"
-                    :class="{ active: isActive(items[4]) }"
-                    @click.prevent="setActive(items[4])"
-                    href="#bank"
-                    >{{ t('person','sachaufwand') }}</a
-                > 
-				<a
-					class="nav-link"
-					:class="{ active: isActive(items[5]) }"
-					@click.prevent="setActive(items[5])"
-					href="#hourly"
-					>{{ t('person', 'stundensaetze') }}</a
-                >
-                <a
-					class="nav-link"
-					:class="{ active: isActive(items[6]) }"
-					@click.prevent="setActive(items[6])"
-					href="#funktion"
-					>{{ t('person', 'funktionen') }}</a
-                >
-            </nav>
-            <div class="tab-content col-md-10" id="nav-tabContent">
-                <div
-                    class="tab-pane"
-                    :class="{ active: isActive(items[0]) }"
-                    role="tabpanel"
-                >
-                    <base-data editMode :personID="currentPersonID" :personUID="currentPersonUID" @updateHeader="updateHeaderHandler"></base-data>
-                </div>
-                <div
-                    class="tab-pane"
-                    :class="{ active: isActive(items[1]) }"
-                    role="tabpanel"
-                >
-                <employee-data editMode :personID="currentPersonID" :personUID="currentPersonUID" @updateHeader="updateHeaderHandler"></employee-data>
-                </div>
-                <div
-                    class="tab-pane"
-                    :class="{ active: isActive(items[2]) }"
-                    role="tabpanel"
-                >
-                    <contact-data editMode :personID="currentPersonID" :personUID="currentPersonUID"></contact-data>
-                </div>
-                <div
-                    class="tab-pane"
-                    :class="{ active: isActive(items[3]) }"
-                    role="tabpanel"
-                >
-                <bank-data editMode :personID="currentPersonID" :personUID="currentPersonUID"></bank-data>
-                </div>
-                <div
-                    class="tab-pane"
-                    :class="{ active: isActive(items[4]) }"
-                    role="tabpanel"
-                >
-                <material-expenses-data editMode :personID="currentPersonID"  :personUID="currentPersonUID"></material-expenses-data>
-                </div>
-				<div
-					class="tab-pane"
-					:class="{ active: isActive(items[5]) }"
-					role="tabpanel"
-				>
-				<hourly-rate-data editMode :personID="currentPersonID"  :personUID="currentPersonUID"></hourly-rate-data>
-				</div>
-                <div
-					class="tab-pane"
-					:class="{ active: isActive(items[6]) }"
-					role="tabpanel"
-				>
-				<job-function editMode :personID="currentPersonID"  :personUID="currentPersonUID" @updateHeader="updateHeaderHandler"></job-function>
-				</div>
-                
-            </div>
-        </div>
+        <fhc-tabs 
+            config-url="/extensions/FHC-Core-Personalverwaltung/apis/TabsConfig/Stammdaten" 
+            style="flex: 1 1 0%; height: 0%"
+            v-model="theModel">
+        </fhc-tabs>
+
       </div>
     </div>
     `
 
 }
+
+export default EmployeePerson;
