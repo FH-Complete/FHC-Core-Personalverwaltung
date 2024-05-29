ffef384757d9a50b7dff101f47172aeec8b935cd
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-05-29 11:33:43 +0200

    make presets overrideable by other extension

---
+++ config/presets.php
@@ -0,0 +1,3 @@
+<?php
+$config['PRESETS_BASEDIR'] = DOC_ROOT . 'application/extensions/FHC-Core-Personalverwaltung/public/js/apps/vbform/presets';
+$config['PRESETS_BASELOCATION'] = BASE_LOCATION . 'public/extensions/FHC-Core-Personalverwaltung/js/apps/vbform/presets';
