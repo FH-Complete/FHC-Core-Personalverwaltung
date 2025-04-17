7ca3dee1583c233a66d1192ef310cf4b2ded1773
Harald Bamberger <harald.bamberger@technikum-wien.at>
2025-04-17 08:22:00 +0200

    dbchanges for tbl_gehaltstyp columns lvexport and lvexport_sum moved to core

--- sql/1/021_gehaltstyp_flags.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE hr.tbl_gehaltstyp ADD COLUMN lvexport boolean not null default false;
-ALTER TABLE hr.tbl_gehaltstyp ADD COLUMN lvexport_sum VARCHAR(256) default null;
-
-UPDATE hr.tbl_gehaltstyp SET lvexport = true WHERE gehaltstyp_kurzbz<>'lohnausgleichatz';
-
-ALTER TABLE INSERT INTO hr.tbl_gehaltstyp(gehaltstyp_kurzbz, bezeichnung, valorisierung, sort,aktiv, lvexport) VALUES('zulage_allin', 'Zulage (allin)', true, 3, true, true); ADD COLUMN lvexport_sum VARCHAR(256) not null default false;
-UPDATE hr.tbl_gehaltstyp set sort = sort + 1 where sort > 3;
-INSERT INTO hr.tbl_gehaltstyp(gehaltstyp_kurzbz, bezeichnung, valorisierung, sort,aktiv, lvexport) VALUES('zulage_allin', 'Zulage (allin)', true, 4, true, true);
-UPDATE hr.tbl_gehaltstyp set lvexport_sum = 'GRUNDGEHALT' where gehaltstyp_kurzbz IN ('grundgehalt', 'zulage_allin');
