80cbfc73d476a7a6f42c2903dd9b1a07066153a1
ma0068 <ma0068@technikum-wien.at>
2025-10-16 11:45:16 +0200

    update EmployeeEditor for using coreDetailHeader

--- public/js/components/employee/EmployeeEditor.js
+++ public/js/components/employee/EmployeeEditor.js
@@ -1,8 +1,5 @@
 import { EmployeePerson } from './EmployeePerson.js';
-import { EmployeeHeader } from './EmployeeHeader.js';
 import { EmployeeNav } from './EmployeeNav.js';
-
-//TEST MANU DETAILHEADER
 import { EmployeeStatus } from './EmployeeStatus.js'
 import CoreDetailsHeader from '../../../../../../public/js/components/DetailHeader/DetailHeader.js';
 import ApiIssue from "../../api/factory/issue.js";
@@ -12,7 +9,6 @@ export default {
 	name: 'EmployeeEditor',
 	components: {
 		EmployeePerson,
-		EmployeeHeader,
 		EmployeeNav,
 		EmployeeStatus,
 		CoreDetailsHeader
@@ -23,62 +19,33 @@ export default {
 		open: Boolean,
 		isNew:  Boolean,
 	},
+	expose: ['refresh'],
 	emits: ['personSelected'],
 	setup( props, {emit }) {
 
-		const router = VueRouter.useRouter();
 		const route = VueRouter.useRoute();
-		//	const { watch, ref, onMounted, inject } = Vue;
 		const currentPersonID = Vue.ref(null);
 		const currentPersonUID = Vue.ref(null);
-		const currentDate = Vue.ref(null);
-		// const employeeHeaderRef = Vue.ref();
 		const employee = Vue.ref();
-
-		//TODO(Manu) check date
-		const formatDate = (ds) => {
-			if (ds == null) return '';
-			var d = new Date(ds);
-			return d?.toISOString().substring(0,10);
-		}
 
 		const isFetching = Vue.ref(false);
 		const isFetchingName = Vue.ref(false);
 		const isFetchingIssues = Vue.ref(false);
 		const openissuescount = Vue.ref(null);
 		const personalnummer = Vue.ref(null);
-
 		const statusRef = Vue.ref(null);
-		const issuesCountRef = Vue.ref(null);
 		const $api = Vue.inject('$api');
-		const $fhcAlert = Vue.inject('$fhcAlert');
 
 		const redirect = (params) => {
 			emit('personSelected', params);
 		}
 
-		const dateChanged = (params) => {
-			console.log("-> date changed: ", params);
-			//currentDate.value=params;
-		}
-
-		const updateHandler = () => {
-			//employeeHeaderRef.value.refresh();
-			console.log("in update handler");
-			//statusRef.value.refresh();
-			refresh();
-
-		}
-
-		const fetchOpenIssuesCount = async() => {
+		const fetchOpenIssuesCount = async(person_id) => {
 			isFetchingIssues.value = true;
 
 			try {
-				const res = await $api.call(ApiIssue.countPersonOpenIssues(props.personid));
+				const res = await $api.call(ApiIssue.countPersonOpenIssues(person_id));
 				openissuescount.value = res.data.openissues;
-				console.log("in  fetchOpenIssuesCount" + res.data.openissues);
-				console.log("in fetchOpenIssuesCount " + props.personid);
-				//	$fhcAlert.alertError("in fetchOpenIssuesCount " + props.personid + " - " + openissuescount.value);
 			} catch (error) {
 				console.log(error);
 			} finally {
@@ -86,17 +53,11 @@ export default {
 			}
 		};
 
-		//Todo(manu) NOT UPDATING, seems to get the value of last person before
 		const checkPerson = async() => {
 			isFetchingIssues.value = true;
-			//console.log("in checkPerson person_id" + personid);
 			try {
 				const res = await $api.call(ApiIssue.checkPerson(props.personid));
 				openissuescount.value = res.data.openissues;
-				console.log("in checkPerson count " + res.data.openissues);
-				console.log("in checkPerson used ID " + props.personid);
-				//	$fhcAlert.alertError("in checkPerson " + props.personid + " - " + openissuescount.value);
-
 			} catch (error) {
 				console.log(error);
 			}
@@ -110,62 +71,34 @@ export default {
 			isFetchingName.value = true;
 			try {
 				// fetch header data
-				console.log("in fetch header data: person_id " + personid + " uid " + personuid);
 				const res = await $api.call(ApiEmployee.personHeaderData(personid, personuid));
 				employee.value = res.data[0];
-				//personalnummer.value = res.data.personalnummer;
-				//const personalnummer = employee.value.personalnummer;
-
 				personalnummer.value = employee.value.personalnummer;
-
-				console.log("employee" + employee.value.personalnummer);
-				console.log("personalnummer " + personalnummer.value);
 				isFetchingName.value = false;
-				// fetch abteilung (needs uid from previous fetch!)
-				//	const resAbteilung = await $api.call(ApiEmployee.personAbteilung(employee.value.uid));
-				// response = await resAbteilung.json();
-				//	employee.value = { ...employee.value, ...{ abteilung: resAbteilung.data } };
 			} catch (error) {
 				console.log(error);
 			}
-			/*	finally {
-					isFetching.value = false;
-					isFetchingName.value = false;
-				}*/
+			finally {
+				isFetching.value = false;
+				isFetchingName.value = false;
+			}
 		};
 
 		const getStatusTags = ()=> {
-			console.log("in getStatusTags");
 			const statusArr = []
-
 			if(employee?.value?.unruly) {
 				statusArr.push({text: 'Unruly', css: 'bg-unruly rounded-0'})
-				console.log("in unruly " + personalnummer.value)
 			}
 			return statusArr
 		}
 
 		const refresh = () => {
-			console.log("in refresh");
-			console.log("person_id " + props.personid + " uid " + props.personuid);
-			console.log("CURRENT person_id " + currentPersonID.value + " uid " + currentPersonUID.value);
-			//issuesCountRef.value.refresh();
-			fetchHeaderData(currentPersonID.value, currentPersonUID.value);
-			fetchOpenIssuesCount(currentPersonID.value);
-			checkPerson(currentPersonID.value);
+			console.log("in refresh 104");
 			statusRef.value.refresh();
 		}
 
 		Vue.onMounted(() => {
-
-			currentDate.value = route.query.d;
-			console.log("date " + currentDate.value);
-			/*			console.log('EmployeeEditor mounted', route.path);
-						currentDate.value = route.query.d;
-						fetchOpenIssuesCount(props.personid);*/
-
 			if (props.personid, props.personuid) {
-				console.log("in on mounted " + props.personid);
 				fetchHeaderData(props.personid, props.personuid);
 				fetchOpenIssuesCount(props.personid);
 				checkPerson(props.personid);
@@ -179,44 +112,19 @@ export default {
 				currentPersonID.value = params.id
 				currentPersonUID.value = params.uid
 
-				// Handle query
-				console.log('test manu')
-				console.log('watch route.query.d', d)
+				if (currentPersonID.value!=null) {
+					fetchHeaderData(currentPersonID.value, currentPersonUID.value);
+					fetchOpenIssuesCount(currentPersonID.value);
+					//checkPerson(currentPersonID.value);
+				}
 				refresh()
-				currentDate.value = d
 			}
 		)
 
-		/*        Vue.watch(
-					() => route.params,
-					params => {
-						currentPersonID.value = params.id;
-						currentPersonUID.value = params.uid;
-					}
-				)
-
-				Vue.watch(
-					() => route.query.d,
-					d => {
-						console.log('test manu');
-						console.log('watch route.query.d', d)
-						currentDate.value = d;
-					}
-				)
-
-				{{employee.value.personalnummer}}
-				<h6 v-if="!isFetchingIssues" class="text-muted">{{ openissuescount }}</h6>
-						<EmployeeHeader ref="employeeHeaderRef"  :personID="personid" :personUID="personuid" @person-selected="redirect"   ></EmployeeHeader>
-				*/
-
 		return {
 			redirect,
-			dateChanged,
 			currentPersonID,
 			currentPersonUID,
-			currentDate,
-			//	employeeHeaderRef,
-			updateHandler,
 			fetchHeaderData,
 			statusRef,
 			getStatusTags,
@@ -227,11 +135,8 @@ export default {
 			employee,
 			isFetching,
 			isFetchingName,
-			formatDate,
 			refresh,
 			personalnummer,
-			issuesCountRef
-
 		}
 	},
 	template: `    
@@ -246,7 +151,7 @@ export default {
         	>
 				<template #issues>
 					<h4 class="mb-1">Issues<a class="refresh-issues" @click="checkPerson"><i class="fas fa-sync"></i></a></h4>
-					<h6 class="text-muted" ref="issuesCountRef">{{ openissuescount }}</h6>
+					<h6 class="text-muted">{{ openissuescount }}</h6>
 				</template>
 				<template #titleAddTile>pNr</template>
 				<template #valueAddTile>{{ personalnummer }}</template>
